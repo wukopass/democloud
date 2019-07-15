966625db0d2b7eb30174f5ff8894150a72853fa1
wyq <1379028478@qq.com>
2019-07-15 09:49:30 +0800

    cloud的第一次提交

---
+++ eureka-client-one/src/main/java/com/example/eurekaclientone/EurekaClientOneApplication.java
@@ -0,0 +1,34 @@
+package com.example.eurekaclientone;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableEurekaClient
+@SpringBootApplication
+@RestController
+public class EurekaClientOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientOneApplication.class, args);
+    }
+
+    @Value("${server.port}")
+    String port;
+
+    @RequestMapping("/hello")
+    public Object home(@RequestParam String name) {
+        Map<String, Object> map = new HashMap<>();
+        String s = "hello" + name + " ,ian  from port:" + port;
+        map.put("srt", s);
+        return map;
+    }
+
+}
