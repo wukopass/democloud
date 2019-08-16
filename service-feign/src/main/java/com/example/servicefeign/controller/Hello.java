16a3227faa32d780119816cb37f780d52e96c899
wukopass <1379028478@qq.com>
2019-08-16 19:10:13 +0800

    调用微信的接口可以了

--- service-feign/src/main/java/com/example/servicefeign/controller/Hello.java
+++ service-feign/src/main/java/com/example/servicefeign/controller/Hello.java
@@ -1,12 +1,13 @@
 package com.example.servicefeign.controller;
 
-import com.quan.peak.model.dto.AlipayDto;
-import com.quan.peak.service.IpayService;
+import com.quan.peak.dto.PayDTO;
+import com.quan.peak.dto.RefundDTO;
+import com.quan.peak.service.IWPayService;
+import feign.Headers;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * \* @Author: Quan
@@ -14,21 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
  * \
  */
 @RestController
-@RequestMapping("alipay")
+@RequestMapping("/wxpay")
 public class Hello {
 
      @Autowired
-     private IpayService ipayService;
+     private IWPayService iwPayService;
 
-
-     @PostMapping(value = "netpay")
-     public String  alipay(AlipayDto alipayDto) {
-         return  ipayService.netPay(alipayDto);
+     @PostMapping(value = "/pay")
+     public String  alipay(PayDTO alipayDto,String tradeType) {
+          return iwPayService.pay( alipayDto, tradeType);
      }
 
-     @PostMapping(value = "wappay")
-     public  String  wappay(AlipayDto alipayDto) {
-          return ipayService.wapPay(alipayDto);
+     @PostMapping(value = "/refund")
+     public String  refund(RefundDTO refundDTO) {
+          return iwPayService.refund(refundDTO);
      }
 
 
